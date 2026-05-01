64214385b4e7539511870d4791f787d0968688e9
Jeremy Fleischman <jeremyfleischman@gmail.com>
2026-05-01 01:55:59 -0700

    Flake input deduplication

--- flake.nix
+++ flake.nix
@@ -12,7 +12,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
